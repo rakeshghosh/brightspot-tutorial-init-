7df6c93e3d54b64c1c778bf9c63fc09b09efae1a
Ryan-Anderson <ryan@perfectsensedigital.com>
2017-01-12 17:00:36 -0500

    Adds Page with header and footer

---
+++ snippet/tutorial/article-page/Step8.snippet
@@ -0,0 +1,9 @@
+package content.page;
+
+import com.psddev.cms.db.PageFilter;
+import com.psddev.cms.view.ViewBinding;
+import com.psddev.dari.db.Recordable;
+
+@ViewBinding(value = PageViewModel.class, types = PageFilter.PAGE_VIEW_TYPE)
+public interface Page extends Recordable {
+}
---
+++ src/main/java/content/page/PageViewModel.java
@@ -0,0 +1,36 @@
+package content.page;
+
+import java.util.logging.Logger;
+
+import com.psddev.cms.view.ViewModel;
+import styleguide.content.page.PageFooterView;
+import styleguide.content.page.PageHeaderView;
+import styleguide.content.page.PageView;
+import styleguide.content.page.PageViewFooterField;
+import styleguide.content.page.PageViewHeaderField;
+import styleguide.content.page.PageViewMainField;
+
+public class PageViewModel extends ViewModel<Page> implements PageView {
+
+    @Override
+    public String getTitle() {
+        return "Brightspot Tutorial";
+    }
+
+    @Override
+    public PageViewHeaderField getHeader() {
+        return new PageHeaderView.Builder().build();
+    }
+
+    @Override
+    public PageViewMainField getMain() {
+        Logger.getAnonymousLogger().info("VIEW: " + model);
+        Logger.getAnonymousLogger().info("VIEW: " + createView(PageViewMainField.class, model));
+        return createView(PageViewMainField.class, model);
+    }
+
+    @Override
+    public PageViewFooterField getFooter() {
+        return new PageFooterView.Builder().build();
+    }
+}
